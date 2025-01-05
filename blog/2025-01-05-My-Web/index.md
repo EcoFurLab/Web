54a1f54f891f3bd96b55195eede43f88645b9909
EcoFur_Git <redstoneblockplayer@gmail.com>
2025-01-05 14:04:45 +0800

    Import

---
+++ blog/2025-01-05-My-Web/index.md
@@ -0,0 +1,7 @@
+---
+slug: 2025-01-05-My-Web
+title: My Web
+authors: [eco]
+tags: [Server]
+---
+Hello
---
+++ docusaurus.config.ts
@@ -0,0 +1,176 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Building!',
+  tagline: 'This web is building',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://ecofur.top/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {
+        type: 'text/javascript',
+      },
+      innerHTML: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "pomr434bjd");`,
+    },
+  ],
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['en', 'zh-Hans'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+        direction: 'ltr',
+      },
+      'zh-Hans': {
+        label: '简体中文',
+        direction: 'ltr',
+      },
+    },
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Eco',
+      logo: {
+        alt: 'Eco\'s Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          // label: 'Tutorial',
+          label: '文档',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          // title: 'Docs',
+          title: '文档',
+          items: [
+            {
+              label: '文档',
+              // label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          // title: 'Community',
+          title: '媒体',
+          items: [
+            {
+              label: 'X',
+              href: 'https://x.com/FurryEco',
+            },
+          ],
+        },
+        {
+          title: '更多',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/ecofur',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Eco Preson.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+  customFields: {
+    translations: {
+      'en': {
+        'footer.docs': 'Docs',
+        'footer.tutorial': '???',
+        'footer.copyright': 'Copyright © ${year} My Company, Inc. Built with Docusaurus.',
+      },
+      'zh-Hans': {
+        'footer.docs': '文档',
+        'footer.tutorial': '教程',
+        'footer.copyright': '版权所有 © ${year} 我的公司。有 Docusaurus 构建。',
+      },
+    },
+  },
+};
+
+export default config;
